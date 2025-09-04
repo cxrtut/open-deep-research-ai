4bba2392ca1c224ef0b4f7389f8dc09d4e735485
M Gift Mukwevho <160480710+cxrtut@users.noreply.github.com>
2025-09-04 10:28:09 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,3 @@
-// import "dotenv/config";
-// import { defineConfig } from "drizzle-kit";
-
-// export default defineConfig({
-//   out: "./drizzle",
-//   schema: "./src/db/schema.ts",
-//   dialect: "postgresql",
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL!,
-//   },
-// });
-
-
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
